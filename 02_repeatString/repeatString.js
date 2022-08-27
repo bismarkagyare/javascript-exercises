9f40db00392c28588d50f80c3d0bc91ac74f5961
Bismark Agyare <bismarkagyare54@gmail.com>
2022-08-27 15:52:57 +0100

    initial commit to repeatString test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let repeatedString = '';
+  while (num > 0) {
+    repeatedString += string;
+    num --;
+  }
+  return repeatedString;
 };
+
+//repeatString("hey", 1);
 
 // Do not edit below this line
 module.exports = repeatString;
