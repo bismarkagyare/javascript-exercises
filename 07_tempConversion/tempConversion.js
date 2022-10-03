6cdef14b5cf582eb2361f6ce31ff0c83b167d6df
Bismark Agyare <bismarkagyare54@gmail.com>
2022-10-03 19:24:40 +0100

    tested temperature conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  // if (temp - 32 && temp * 5 && temp / 9) {
+  //   return null;
+  // } else {
+  //   return;
+  // }
+  return Math.round((temp - 32) * (5 / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  // if (temp / 5 && temp * 9 && temp + 9) {
+  //   return 32;
+  // } else {
+  //   return;
+  // }
+  return Math.round(((temp * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
